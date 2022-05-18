ad95e6cfa0af9d3aaad06f57775220a7a881afb6
Aiden5486Pearce <60283775+Aiden5486Pearce@users.noreply.github.com>
2022-05-18 11:15:25 +0530

    Update product.js

--- controler/product.js
+++ controler/product.js
@@ -1,1 +1,21 @@
+const products = [];
+exports.getAddProduct = (req, res, next) => {
+    console.log('Hi from here');
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product'
+    });
+};
 
+exports.postAddProduct = (req, res, next) => {
+    products.push({ title: req.body.title });
+    res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+    res.render('shop', {
+        prod: products,
+        pageTitle: 'Shop',
+        path: '/'
+    });
+};

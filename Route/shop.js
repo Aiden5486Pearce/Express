76701922520f0538e0fe674c410e0822c8d803d2
Aiden5486Pearce <60283775+Aiden5486Pearce@users.noreply.github.com>
2022-05-18 18:45:43 +0530

    shop.js

---
+++ Route/shop.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+//const rootDir = require('../util/path');
+const productsController = require('../controler/product');
+const router = express.Router();
+
+router.get('/', productsController.getProducts);
+
+/*router.get('/', (req,res,next)=>{
+    res.sendFile(path.join(rootDir,'views','shop.html'));
+});*/
+
+module.exports = router;
+

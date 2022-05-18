93aeecc1fea07fc85dad34623307a5270b9bdfd3
Aiden5486Pearce <60283775+Aiden5486Pearce@users.noreply.github.com>
2022-05-18 11:13:44 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,18 @@
+const path = require('path');
+const express = require('express');
+//const rootDir = require('../util/path');
+const productsController = require('../controler/product');
+const router = express.Router();
+//const product = express.product;
 
+router.get('/add-product', productsController.getAddProduct);
+/*router.get('/add-product', (req,res,next)=>{
+    console.log('Hi from here');
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
+    
+});*/
+
+router.post('/add-product', productsController.postAddProduct);
+
+module.exports = router;
+//exports.product = product;
